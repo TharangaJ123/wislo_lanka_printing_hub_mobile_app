3c8678313bb3774fc91eda234c307628c88c5c50
Bhagya Adikari <kumarini.adikari@gmail.com>
2025-11-26 11:27:51 +0530

    screen order fixed

--- wislo-lanka/App.js
+++ wislo-lanka/App.js
@@ -1,14 +1,43 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import AppNavigator from './src/navigation/AppNavigator';
+import "react-native-gesture-handler";
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import AppNavigator from "./src/navigation/AppNavigator";
+import SplashScreen from "./src/screens/SplashScreen";
+import OnboardingScreen from "./src/screens/OnboardingScreen";
+import AuthScreen from "./src/screens/AuthScreen";
 
 export default function App() {
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <AppNavigator />
-      <StatusBar style="dark" />
-    </GestureHandlerRootView>
-  );
+  const [stage, setStage] = useState("splash");
+
+  const handleSplashFinish = () => setStage("onboarding");
+  const handleOnboardingFinish = () => setStage("auth");
+  const handleAuthContinue = () => setStage("app");
+
+  let content = null;
+  switch (stage) {
+    case "splash":
+      content = <SplashScreen onFinish={handleSplashFinish} />;
+      break;
+    case "onboarding":
+      content = (
+        <OnboardingScreen
+          onSkip={handleOnboardingFinish}
+          onDone={handleOnboardingFinish}
+        />
+      );
+      break;
+    case "auth":
+      content = <AuthScreen onContinue={handleAuthContinue} />;
+      break;
+    default:
+      content = (
+        <>
+          <AppNavigator />
+          <StatusBar style="dark" />
+        </>
+      );
+  }
+
+  return <GestureHandlerRootView style={{ flex: 1 }}>{content}</GestureHandlerRootView>;
 }
